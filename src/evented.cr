d8cc347c82dff6a558f2f53cfdf1ac6b4f336ded
Kris Leech <kris.leech@gmail.com>
2015-07-29 22:39:34 +0100

    WIP

--- src/evented.cr
+++ src/evented.cr
@@ -3,11 +3,14 @@ require "./evented/*"
 module Evented
   module Publisher
     def subscribe(listener)
-
+      listeners << listener
     end
 
     def broadcast(event_name, *args)
+    end
 
+    def listeners
+      @listeners ||= []
     end
   end
 end
