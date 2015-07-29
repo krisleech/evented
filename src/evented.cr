03f9f560e109b2c97900caf75a3df4efc9adb3fd
Kris Leech <kris.leech@gmail.com>
2015-07-29 22:36:16 +0100

    Add module with expected methods.

--- src/evented.cr
+++ src/evented.cr
@@ -1,5 +1,13 @@
 require "./evented/*"
 
 module Evented
-  # TODO Put your code here
+  module Publisher
+    def subscribe(listener)
+
+    end
+
+    def broadcast(event_name, *args)
+
+    end
+  end
 end
